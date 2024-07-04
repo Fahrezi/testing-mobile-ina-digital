d39cc63b8dbd6358675c07fb79241213a3045352
Umar Faruq <umar.faruq@digitalperuri.id>
2024-07-04 14:19:08 +0700

    test BPJS API

--- src/lib/hooks/useMemberList.ts
+++ src/lib/hooks/useMemberList.ts
@@ -1,14 +1,14 @@
-import { useCallback, useEffect, useReducer, useState } from 'react';
-import { APIResponse, Member } from '../definitions';
+import { useCallback, useEffect, useReducer, useState } from "react";
+import { APIResponse, Member } from "../definitions";
 
 export function useMemberList(): {
   isLoading: boolean;
   response: APIResponse<Member[]>;
 } {
   const initialResponse: APIResponse<Member[]> = {
-    status: 'default',
+    status: "default",
     data: [],
-    message: '',
+    message: "",
   };
   type ActionType = {
     type: string;
@@ -19,21 +19,21 @@ export function useMemberList(): {
   };
   function reducer(
     state: APIResponse<Member[]>,
-    actionType: ActionType
+    actionType: ActionType,
   ): APIResponse<Member[]> {
     const { type } = actionType;
     switch (type) {
-      case 'success':
+      case "success":
         return {
           ...state,
           ...actionType.payload,
         };
-      case 'error':
+      case "error":
         return {
           ...state,
-          status: 'error',
+          status: "error",
           message:
-            'Terjadi kesalahan pada sistem. <br/> Silahkan coba beberapa saat lagi',
+            "Terjadi kesalahan pada sistem. <br/> Silahkan coba beberapa saat lagi",
           data: [],
         };
 
@@ -47,20 +47,34 @@ export function useMemberList(): {
   const getData = useCallback(async (userId: string) => {
     setIsLoading(true);
     try {
+      const resBpjsTk = await fetch(
+        `
+https://www.bpjsketenagakerjaan.go.id/plkk-kontak`,
+        {
+          method: "POST",
+          body: JSON.stringify({
+            kodeKabupaten: "3174",
+            page: 1,
+          }),
+        },
+      );
       const res = await fetch(
-        `${process.env.API_BACKEND}/kepesertaan/${userId}`
+        `${process.env.API_BACKEND}/kepesertaan/${userId}`,
       );
       const resJson = await res.json();
-      dispatchResponse({ type: 'success', payload: resJson });
+
+      const resJsonTK = await resBpjsTk.json();
+      dispatchResponse({ type: "success", payload: resJson });
     } catch (e) {
-      dispatchResponse({ type: 'error', payload: { data: [] } });
+      dispatchResponse({ type: "error", payload: { data: [] } });
+      console.log("error : ", e);
     } finally {
       setIsLoading(false);
     }
   }, []);
   useEffect(() => {
     // it will be userId from authentication
-    const userId = '001';
+    const userId = "001";
     getData(userId);
     return () => {};
   }, [getData]);
